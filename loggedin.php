d2807f9f129503b492d80e8e2a6a5e84aa27fb99
justincalingasan <calingasanjustin@gmail.com>
2014-10-13 17:57:21 +0800

    Header and Footer changes
    -added Dashboard link to user menu
    -changed footer background to dark blue

--- loggedin.php
+++ loggedin.php
@@ -1,5 +1,6 @@
 								<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> Username<span class="caret"></span></a>
 									<ul class="dropdown-menu" role="menu">
+										<li><a href="#">Dashboard</a></li>
 										<li><a href="#">Events</a></li>
 										<li><a href="#">Organizations</a></li>
 										<li><a href="#">Profile</a></li>
