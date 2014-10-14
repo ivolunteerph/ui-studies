20f141501b66611f8d568c4118d283098b774f88
justincalingasan <calingasanjustin@gmail.com>
2014-10-14 20:07:50 +0800

    Logo change - tagline
    added tagline or “strapline” as indicated in brand bible

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 							</button>
-							<a class="navbar-brand" href="#"><img src="images/ivolunteer-logo-240.png" alt="iVolunteer Philippines"></a>
+							<a class="navbar-brand" href="#"><img src="images/ivolunteer-logo-240-58.png" alt="iVolunteer Philippines"></a>
 						</div>
 					
 						<!-- Collect the nav links, forms, and other content for toggling -->
