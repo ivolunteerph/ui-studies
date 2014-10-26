c9864f74b224f2aefafc4854689590bda1f64e85
justincalingasan <calingasanjustin@gmail.com>
2014-10-26 17:41:37 +0800

    Manage Orgs Flow
    -changes in dashboard
    -create org page
    -orgs list page
    
    to follow:
    view org page

--- dashboard.php
+++ dashboard.php
@@ -48,11 +48,25 @@
 				        </div>
 			        </div>
 			        <hr>
-			        <div class="events">
+			        <div class="orgs">
 				        <h2 class="head">Organizations</h2>
 				        <div class="alert alert-info">
 				        	You don't have any organizations.
 				        </div>
+				        <div class="org-list linear-list">
+				        	<div class="list-item">
+								<a href="">
+										<span class="item-image org-logo"><img src="http://ivolunteer.com.ph/system/organization_images/79/small/AHON_LOGO_edited-1.png?1399602838"></span>
+										<span class="item-title org-name">AHON (Acts of Hope for the Nation) Foundation</span> 
+								</a>
+				        	</div>
+				        	<div class="list-item">
+								<a href="">
+									<span class="item-image org-logo"><img src="http://ivolunteer.com.ph/system/organization_images/78/small/passive_income_designers.jpg?1399478387"></span>
+									<span class="item-title org-name">Exponential Wealth Consultancy</span> 
+								</a>
+				        	</div>
+				        </div><!--Org list-->
 			        </div>
 			        <hr>
 				</div>
---
+++ orgs.php
@@ -0,0 +1,26 @@
+		<div class="container">
+			<div class="col-md-12">
+				<h1 class="head"><a href="" class="btn btn-default btn-sm"><i class="fa fa-rotate-270 fa-level-up"></i></a> Organizations</h1>
+				<div id="orgs" class="user orgs">
+				        <div class="alert alert-info">
+				        	You don't have any organizations.
+				        </div>
+				        <div class="org-list linear-list">
+				        	<div class="list-item">
+								<a href="">
+									<span class="item-image org-logo"><img src="http://ivolunteer.com.ph/system/organization_images/79/small/AHON_LOGO_edited-1.png?1399602838"></span>
+									<span class="item-title org-name">AHON (Acts of Hope for the Nation) Foundation</span> 
+								</a>
+				        	</div>
+				        	<div class="list-item">
+								<a href="">
+									<span class="item-image org-logo"><img src="http://ivolunteer.com.ph/system/organization_images/78/small/passive_income_designers.jpg?1399478387"></span>
+									<span class="item-title org-name">Exponential Wealth Consultancy</span> 
+								</a>
+				        	</div>
+				        </div><!--Org list-->
+			        </div>
+			        <hr>
+				</div>
+	        </div>
+		</div>
