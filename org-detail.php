e0453756bd90b54fcf13dcc3c0eeb7f4c6000cbc
justincalingasan <calingasanjustin@gmail.com>
2014-10-26 21:32:46 +0800

    View Organization page

---
+++ org-detail.php
@@ -0,0 +1,56 @@
+		<div class="org-detail">
+			<div class="container">
+				<div class="col-md-12">
+					<ol class="breadcrumb">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Organtizations</a></li>
+						<li class="active">Caucus of Development NGO Networks (CODE-NGO) </li>
+					</ol>
+					<h1 class="head"> Caucus of Development NGO Networks (CODE-NGO)</h1>
+				</div>
+				<div class="col-md-3">
+					<div class="detail-img">
+						<img class="img-responsive" src="http://ivolunteer.com.ph/system/organization_images/64/small/tall.jpg?1384228117" alt="Feeding, BraCaucus of Development NGO Networks (CODE-NGO)">
+
+					</div><!--/.detail-img-->
+					<div class="short-details">
+						<ul class="details-list">
+			                <li class="detail-item org-location item-location"><i class="fa fa-fw fa-location-arrow"></i> 69 Esteban Abada Street, Loyola Heights 1108 Quezon City </li>
+			                <li class="detail-item org-country item-country"><i class="fa fa-fw fa-globe"></i> Philippines </li>			           	
+			                <li class="detail-item org-categories item-categories"><i class="fa fa-fw fa-list"></i> Education, Poverty, Environment  </li>	
+						</ul>
+					</div><!--/.short-details-->
+				</div>
+				
+				<div class="col-md-5">
+					<div class="description">
+						<h2 class="">Mission statement</h2>
+						<p class="lead">To be a trusted national voice of civil society and to advance the capacities of CSOs to exercise transformative leadership.</p>
+						<hr>
+						<p>The Caucus of Development NGO Networks (CODE-NGO) is the country’s biggest coalition of civil society organizations working for social development, with its 6 national networks and 6 regional networks representing more than 1,600 development NGOs, POs and cooperatives nationwide. CODE-NGO was established in 1990 by the 10 largest NGO networks in the Philippines. It focuses on being one of the trusted national voices of civil society and on advancing the capacities of civil society organizations (CSOs) to exercise transformative leadership. CODE-NGO’s priority advocacies are transparency, accountability, integrity and people’s participation in governance, asset reform, environmental protection, accessible and high quality social services, CSO good governance, official development aid (ODA) reform, and federalism and constitutional reform. </p>
+						
+						
+						<p><em>We also encourage you to buy our iVolunteer Philippines T-shirt to support our volunteerism portal. Just contact Hazel at +639285521415 or through contactus@ivolunteer.com.ph. Thanks!</em></p>
+					</div>
+				</div>
+				<div class="col-md-4 ">
+					<div class="org-contact panel panel-default">
+						<div class="panel-body">
+							<i class="fa fa-calendar"></i> <a href="">View events by this organization</a>
+							<p><a href="" class="btn btn-block btn-info">Edit Organization</a></p>
+						</div>
+						
+					</div>
+					<dl class="dl-horizontal">
+						  <dt>Contact Person</dt>
+						  <dd>Celia de Jesus</dd>
+						  <dt>Contact Number</dt>
+						  <dd> 09177342259</dd>
+						  <dt>Email</dt>
+						  <dd>cdejesus@code-ngo.org</dd>
+						  <dt>Website</dt>
+						  <dd>www.code-ngo.org</dd>
+						</dl>
+				</div>
+			</div>
+		</div>
