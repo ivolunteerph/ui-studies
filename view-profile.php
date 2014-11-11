84a7e44ce056dc514e20cbfef903dae99f96958d
justincalingasan <calingasanjustin@gmail.com>
2014-11-11 22:31:54 +0800

    View Profile Page

---
+++ view-profile.php
@@ -0,0 +1,38 @@
+		<div class="container">
+			<div class="col-md-6 col-md-offset-3">
+				<h1 class="head">Profile </h1>
+				<div id="viewProfile" class="user view-profile">
+					<div class="well">
+						<h2>FirstName LastName <a href="" class="btn btn-default btn-sm"><i class="fa fa-wrench"></i> Edit Profile</a></h2>
+					    <dl class="profile-list dl-horizontal">
+						    <dt>Birthday:</dt>
+						    <dd>1/1/1979</dd>
+						    <dt>Expertise:</dt>
+						    <dd>
+						    	<ul class="expertise list list-unstyled">
+							    	<li>Computing</li>
+							    	<li>Expertise 2</li>
+						    	</ul>
+						    </dd>
+							<dt>Country:</dt>
+							<dd>Philippines </dd>
+							<dt>Region:</dt>
+							<dd>NCR</dd>
+							<dt>City:</dt>
+							<dd>Quezon City</dd>
+							<dt>Nationality:</dt>
+							<dd>Philippines </dd>
+							<dt>Gender:</dt>
+							<dd>Male </dd>
+							<dt>Organizations:</dt>
+							<dd>
+								<ul class="expertise list list-unstyled">
+									<li><a href="">Organization Name</a></li>
+									<li><a href="">Organization 2 Name</a></li>
+								</ul>
+							</dd>
+					    </dl>
+					</div>
+				</div>
+	        </div>
+		</div>
