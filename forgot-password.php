fcb576928e8946ed846e8e5c9e7f0fca31038ffc
justincalingasan <calingasanjustin@gmail.com>
2014-11-16 19:28:03 +0800

    Forgot Password Page
    to view:
    index.php?file=forgot-password&single=true
    
    Should look almost like the login page, might also need copy to explain
    what the user should expect. Maybe like: “Enter the  email you used to
    register to get a reset password link”?

---
+++ forgot-password.php
@@ -0,0 +1,20 @@
+<div class="container">
+	<div class="col-md-6 col-md-offset-3">
+		<h1 class="head">Forgot Password</h1>
+		<div class="alert alert-danger">
+			We can't find that email in the system.
+		</div>
+		<div class="login well">
+			<form>
+				
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="text" placeholder="email@address.com" id="email" class="form-control input-lg">
+				</div>
+				<div class="form-group">
+					<button type="button" class="btn btn-primary">Reset Password</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
