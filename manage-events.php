7397c8f5c2ea8564a886746a6508848e329b8704
justincalingasan <calingasanjustin@gmail.com>
2014-11-16 19:17:34 +0800

    Manage Events Page

---
+++ manage-events.php
@@ -0,0 +1,74 @@
+		<div class="container">
+			<div class="col-md-12">
+				<h1 class="head"><a href="" class="btn btn-default btn-sm"><i class="fa fa-rotate-270 fa-level-up"></i></a> Manage Events <!--<a href="" class="btn btn-default btn-sm">Manage</a>--><a href="" class="btn btn-sm btn-primary">Create <i class="fa fa-plus"></i></a></h1>
+				<div id="events" class="user manage-events events">
+			        <div class="alert alert-info">
+			        	You don't have any events yet. <a href="">Create an event</a>
+			        </div>
+			        <div class="event-list linear-list">
+			        	<div class="list-item">
+			        		<div class="row">
+								<div class="col-md-10 col-sm-12">
+									<a href=""><span class="item-title">
+										Christmas with National Children’s Hospital</span>
+										<small><span class="vo-location item-location"><i class="fa fa-fw fa-location-arrow"></i> Helping Land Tondo Manila </span>
+										<span class="vo-date item-date"><i class="fa fa-fw fa-calendar"></i> Sept 6, 2014, 07:00 AM - 12:30 PM </span></small>
+									</a>
+								</div>
+								<div class="col-md-2 col-sm-12">
+									<div class="action-btns">
+										<button class="btn-block btn-sm btn btn-default">Edit <i class="fa fa-wrench"></i></button>
+										<button class="btn-block  btn-sm btn btn-default">Delete <i class="fa fa-times-circle text-danger"></i></button>
+									</div>
+								</div>
+							</div>								
+						</div><!--/.list-item-->
+			        	<div class="list-item">
+			        		<div class="row">
+								<div class="col-md-10 col-sm-12">
+									<a href=""><span class="item-title">
+										[IVOLUNTEER DAY] Day at the Museum</span>
+										<small><span class="vo-location item-location"><i class="fa fa-fw fa-location-arrow"></i> Helping Land Tondo Manila </span>
+										<span class="vo-date item-date"><i class="fa fa-fw fa-calendar"></i> Sept 6, 2014, 07:00 AM - 12:30 PM </span></small>
+									</a>
+								</div>
+								<div class="col-md-2 col-sm-12">
+									<div class="action-btns">
+										<button class="btn-block btn-sm btn btn-default">Edit <i class="fa fa-wrench"></i></button>
+										<button class="btn-block  btn-sm btn btn-default">Delete <i class="fa fa-times-circle text-danger"></i></button>
+									</div>
+								</div>
+							</div>								
+						</div><!--/.list-item-->
+			        	<div class="list-item">
+			        		<div class="row">
+								<div class="col-md-10 col-sm-12">
+									<a href=""><span class="item-title">
+										Christmas with National Children’s Hospital</span>
+										<small><span class="vo-location item-location"><i class="fa fa-fw fa-location-arrow"></i> Helping Land Tondo Manila </span>
+										<span class="vo-date item-date"><i class="fa fa-fw fa-calendar"></i> Sept 6, 2014, 07:00 AM - 12:30 PM </span></small>
+									</a>
+								</div>
+								<div class="col-md-2 col-sm-12">
+									<div class="action-btns">
+										<button class="btn-block btn-sm btn btn-default">Edit <i class="fa fa-wrench"></i></button>
+										<button class="btn-block  btn-sm btn btn-default">Delete <i class="fa fa-times-circle text-danger"></i></button>
+									</div>
+								</div>
+							</div>								
+						</div><!--/.list-item-->
+			        </div><!--/.event-list-->
+					
+					<ul class="pagination">
+					  <li><a href="#">&laquo;</a></li>
+					  <li><a href="#" class="active">1</a></li>
+					  <li><a href="#">2</a></li>
+					  <li><a href="#">3</a></li>
+					  <li><a href="#">4</a></li>
+					  <li><a href="#">5</a></li>
+					  <li><a href="#">&raquo;</a></li>
+					</ul><!--/.pagination-->
+					
+				</div><!--/#events.user.joined-events.events-->
+	        </div><!--/.col-md-12-->
+		</div><!--/.container-->
